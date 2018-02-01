faaa62c8f8ae1b458b5588b1aa3823913a6cf349
yf <iyuanfang0105@gmail.com>
2018-02-01 23:34:44 +0800

    mtcnn class and face detect from camera

--- applications/mtcnn/mtcnn_test.py
+++ applications/mtcnn/mtcnn_test.py
@@ -1,29 +1,16 @@
 # coding=utf-8
 import tensorflow as tf
-import detect_face
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-minsize = 20  # minimum size of face
-threshold = [0.6, 0.7, 0.7]  # three steps's threshold
-factor = 0.709
+import MTCNN_face_detector
 
 
-def mtcnn_face_detection_from_image(image):
-    '''
-    face detection using mtcnn
-    :param image: 3-D ararry
-    :return:
-    '''
-    # image = cv2.imread(img_path)
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    with tf.Session() as sess:
-        pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
-        bounding_boxes, landmarks = detect_face.detect_face(image, minsize, pnet, rnet, onet, threshold, factor)
+def face_detect_from_image(mtcnn_detector, image):
+    bounding_boxes, landmarks = mtcnn_detector.detect_face(image)
     faces_num = bounding_boxes.shape[0]  # number of faces
-    # print "====>>>> Found faces：" + str(faces_num)
+    print "====>>>> Found faces：" + str(faces_num)
     faces = []
     points = []
     for face_position, landmark_position in zip(bounding_boxes, np.transpose(landmarks)):
@@ -38,16 +25,18 @@ def mtcnn_face_detection_from_image(image):
     return faces, points
 
 
-def mtcnn_face_detection_from_camera():
+def face_detect_from_camera(mtcnn_detector):
     cap = cv2.VideoCapture(0)
+    count = 0
     while True:
+        count = count + 1
         # Capture frame-by-frame
         ret, frame = cap.read()
         # Our operations on the frame come here
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces, points = mtcnn_face_detection_from_image(frame)
-        # Display the resulting frame
-        plot_faces_landmarks(frame, faces, points, plt_flag=False)
+        if count % 2 == 0:
+            faces, points = face_detect_from_image(mtcnn_detector, frame)
+            plot_faces_landmarks(frame, faces, points, plt_flag=False)
+
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -67,7 +56,7 @@ def plot_faces_landmarks(image, faces, points, plt_flag=True):
     for face, point in zip(faces, points):
         cv2.rectangle(image, (face[0], face[1]), (face[2], face[3]), (0, 255, 0), 2)
         for p in point:
-            cv2.circle(image, (p[0], p[1]), 2, (255, 0, 0), 2)
+            cv2.circle(image, (p[0], p[1]), 4, (0, 0, 255), 2)
     if plt_flag:
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         plt.show()
@@ -76,6 +65,7 @@ def plot_faces_landmarks(image, faces, points, plt_flag=True):
 if __name__ == '__main__':
     image_path = "../../images/faces.jpg"
     image = cv2.imread(image_path)
-    faces, points = mtcnn_face_detection_from_image(image)
-    plot_faces_landmarks(image, faces, points)
-    print flag
+    mtcnn_detector = MTCNN_face_detector.MTCNN()
+    # faces, points = face_detect_from_image(mtcnn_detector, image)
+    # plot_faces_landmarks(image, faces, points)
+    face_detect_from_camera(mtcnn_detector)
